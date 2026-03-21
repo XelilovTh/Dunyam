7532a927d8ac57b9a29cd6847bf6407f9ad0abcd
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2026-03-22 01:17:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const START_DATE    = new Date('2023-02-01');
 const MAIN_PAGE_URL = 'main.html';
 
 // GitHub konfiqurasiyası
-const GITHUB_TOKEN   = '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';
+const GITHUB_TOKEN   = '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';
 const GITHUB_OWNER = 'XelilovTh';
 const GITHUB_REPO  = 'Dunyam';
 
