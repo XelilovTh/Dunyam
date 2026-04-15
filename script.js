732cc8a1b725be7730adaa0e5b9c0d5d0109893b
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2026-04-15 16:40:22 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const API_HEADERS = {
 };
 
 const APP_CONFIG = {
-    password: '2023',
+    password: '',
     startDate: new Date('2023-02-01T00:00:00'),
     version: '2.1.0',
     maxFileSize: {
