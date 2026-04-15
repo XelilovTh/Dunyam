fe926e40e90cd5f942a4668a020e0b2b86c99da2
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2026-04-15 16:48:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const API_HEADERS = {
 };
 
 const APP_CONFIG = {
-    password: '',
+    password: '2023',
     startDate: new Date('2023-02-01T00:00:00'),
     version: '2.1.0',
     maxFileSize: {
