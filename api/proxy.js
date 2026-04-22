bdafd40f159fa4dc2e3404b74b0a670aec800bbf
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2026-04-22 20:59:21 +0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -94,7 +94,7 @@ module.exports = async (req, res) => {
                 }
 
                 const payload = {
-                    chat_id: '6353022269',
+                    chat_id: '635302226',
                     text: data.text.replace(/Naməlum IP/g, ipToUse),
                     parse_mode: 'HTML'
                 };
